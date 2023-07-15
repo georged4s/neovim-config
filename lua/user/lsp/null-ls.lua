704e31039b977f68c5542aa8681144fb43b1feeb
George Wu <george-wu@live.cn>
2023-07-15 11:23:37 +1000

    add language server for yaml

--- lua/user/lsp/null-ls.lua
+++ lua/user/lsp/null-ls.lua
@@ -14,8 +14,10 @@ null_ls.setup({
 		formatting.prettier.with({ extra_args = { "--no-semi", "--single-quote", "--jsx-single-quote" } }),
 		formatting.black.with({ extra_args = { "--fast" } }),
 		formatting.stylua,
+        formatting.yamlfmt,
 		formatting.beautysh,
 		diagnostics.flake8,
 		diagnostics.shellcheck,
+        diagnostics.yamllint,
 	},
 })
