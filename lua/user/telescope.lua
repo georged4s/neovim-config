d46989007f05dfa3648c7aa565b8fb26c7991057
George Wu <george-wu@live.cn>
2024-01-04 01:47:00 +1100

    reenable fzf for file search

--- lua/user/telescope.lua
+++ lua/user/telescope.lua
@@ -93,13 +93,12 @@ telescope.setup({
         --   extension_config_key = value,
         -- }
         -- please take a look at the readme of the extension you want to configure
-        -- fzf = {
-        -- 	fuzzy = true, -- false will only do exact matching
-        -- 	override_generic_sorter = true, -- override the generic sorter
-        -- 	override_file_sorter = true, -- override the file sorter
-        -- 	case_mode = "smart_case", -- or "ignore_case" or "respect_case"
-        -- the default case_mode is "smart_case"
-        -- },
+        fzf = {
+            fuzzy = true, -- false will only do exact matching
+            override_generic_sorter = true, -- override the generic sorter
+            override_file_sorter = true, -- override the file sorter
+            case_mode = "smart_case", -- or "ignore_case" or "respect_case". the default case_mode is "smart_case"
+        },
         live_grep_args = {
             prompt_title = "\\Search in Current Workspace/",
             theme = "ivy",
@@ -120,4 +119,4 @@ telescope.setup({
 
 -- Enable telescope extensions
 pcall(telescope.load_extension, "live_grep_args")
--- pcall(telescope.load_extension, "fzf")
+pcall(telescope.load_extension, "fzf")
