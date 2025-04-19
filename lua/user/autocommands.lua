aa659de363d0e6c4f9bdbe82b252a2b3de7c7ecf
George Wu <george-wu@live.cn>
2025-04-19 19:08:33 +1000

    feat: add lsp related autocommand

--- lua/user/autocommands.lua
+++ lua/user/autocommands.lua
@@ -72,6 +72,82 @@ vim.api.nvim_create_autocmd({ "FileType" }, {
     pattern = { "fugitive", "git" },
 })
 
+-- LSP
+local _lsp = vim.api.nvim_create_augroup("LSP", { clear = true })
+vim.api.nvim_create_autocmd("LspAttach", {
+    group = _lsp,
+    callback = function(event)
+        local map = function(keys, func, desc, mode)
+            mode = mode or "n"
+            vim.keymap.set(mode, keys, func, { buffer = event.buf, desc = "[LSP]: " .. desc })
+        end
+
+        -- Common LSP usage and servers information
+        map("<leader>lf", function()
+            vim.lsp.buf.format({
+                filter = function(client)
+                    return client.name == "null-ls"
+                end,
+            })
+        end, "Format")
+        map("<leader>li", "<cmd>LspInfo<cr>", "Server Info")
+        map("<leader>lm", "<cmd>Mason<cr>", "Mason")
+        map("<leader>ln", "<cmd>NullLsInfo<cr>", "NullLs")
+        map("<leader>lh", function()
+            vim.lsp.inlay_hint.enable(not vim.lsp.inlay_hint.is_enabled({ bufnr = event.buf }))
+        end, "Toggle Inlay Hints")
+
+        -- Conditionally add language specific keymaps:
+        -- sqls
+        local client = vim.lsp.get_client_by_id(event.data.client_id)
+        if client and client.name == "sqls" then
+            map("<leader><cr>", "<Plug>(sqls-execute-query)", "Execute SQL", { "n", "v" })
+        end
+
+        -- Navigate between diagnostics
+        map("<leader>lj", "<cmd>lua vim.diagnostic.jump({ count = 1, float = true })<cr>", "Goto Next Diagnostic")
+        map("<leader>lk", "<cmd>lua vim.diagnostic.jump({ count = -1, float = true })<cr>", "Goto Prev Diagnostic")
+
+        -- Open diagnostics in a floatting window
+        map("gl", "<cmd>lua vim.diagnostic.open_float()<cr>", "[L]ine Diagnostics")
+
+        -- Rename the variable under your cursor.
+        --  Most Language Servers support renaming across files, etc.
+        map("grn", vim.lsp.buf.rename, "[R]e[n]ame")
+
+        -- Execute a code action, usually your cursor needs to be on top of an error
+        -- or a suggestion from your LSP for this to activate.
+        map("gra", vim.lsp.buf.code_action, "[G]oto Code [A]ction", { "n", "x" })
+
+        -- Find references for the word under your cursor.
+        map("gr", require("telescope.builtin").lsp_references, "[G]oto [R]eferences")
+
+        -- Jump to the implementation of the word under your cursor.
+        --  Useful when your language has ways of declaring types without an actual implementation.
+        map("gri", require("telescope.builtin").lsp_implementations, "[G]oto [I]mplementation")
+
+        -- Jump to the definition of the word under your cursor.
+        --  This is where a variable was first declared, or where a function is defined, etc.
+        --  To jump back, press <C-t>.
+        map("gd", require("telescope.builtin").lsp_definitions, "[G]oto [D]efinition")
+
+        -- WARN: This is not Goto Definition, this is Goto Declaration.
+        --  For example, in C this would take you to the header.
+        map("grD", vim.lsp.buf.declaration, "[G]oto [D]eclaration")
+
+        -- Fuzzy find all the symbols in your current document.
+        --  Symbols are things like variables, functions, types, etc.
+        map("gO", require("telescope.builtin").lsp_document_symbols, "Open Document Symbols")
+
+        -- Fuzzy find all the symbols in your current workspace.
+        --  Similar to document symbols, except searches over your entire project.
+        map("gW", require("telescope.builtin").lsp_dynamic_workspace_symbols, "Open Workspace Symbols")
+
+        -- Jump to the type of the word under your cursor.
+        --  Useful when you're not sure what type a variable is and you want to see
+        --  the definition of its *type*, not where it was *defined*.
+        map("grt", require("telescope.builtin").lsp_type_definitions, "[G]oto [T]ype Definition")
+    end,
 })
 
 -- Markdown
