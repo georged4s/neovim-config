03790b4f2f544002c23ba5cdaa1677389ea69e63
George Wu <george-wu@live.cn>
2025-04-19 17:33:07 +1000

    refactor plugin chafa

---
+++ lua/config/plugins/chafa.lua
@@ -0,0 +1,14 @@
+return {
+    {
+        "princejoogie/chafa.nvim",
+        opts = {
+            render = {
+                min_padding = 5,
+                show_label = true,
+            },
+            events = {
+                update_on_nvim_resize = true,
+            },
+        },
+    },
+}
