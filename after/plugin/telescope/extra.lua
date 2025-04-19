4e34860390baa9542ad22da1a53c8f2bf91b4c61
George Wu <george-wu@live.cn>
2025-04-19 17:39:55 +1000

    format file

--- after/plugin/telescope/extra.lua
+++ after/plugin/telescope/extra.lua
@@ -1,13 +1,15 @@
 local M = {}
 
+-- define custom function to search in a git repo
+-- if .git/ exists or otherwise trigger built-in find_files
 M.project_files = function()
-	local opts = {}
-	vim.fn.system("git rev-parse --is-inside-work-tree")
-	if vim.v.shell_error == 0 then
-		require("telescope.builtin").git_files(opts)
-	else
-		require("telescope.builtin").find_files(opts)
-	end
+    local opts = {}
+    vim.fn.system("git rev-parse --is-inside-work-tree")
+    if vim.v.shell_error == 0 then
+        require("telescope.builtin").git_files(opts)
+    else
+        require("telescope.builtin").find_files(opts)
+    end
 end
 
 return M
