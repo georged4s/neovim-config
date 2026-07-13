9d64f38ecb769f639bb409315995bb6294bc2c70
George Wu <george-wu@live.cn>
2026-07-13 09:05:09 +0800

    feat(lsp): add xmlformat via formatter_factory
    Replace commented-out formatting.xmlformatter with working
    null-ls.helpers.formatter_factory pointing to xmlformat.

--- lua/config/plugins/lsp/none-ls.lua
+++ lua/config/plugins/lsp/none-ls.lua
@@ -6,8 +6,10 @@ return {
 			local null_ls = require("null-ls")
 			local formatting = null_ls.builtins.formatting
 			local diagnostics = null_ls.builtins.diagnostics
+			local helpers = require("null-ls.helpers")
 			null_ls.setup({
 				sources = {
+					--builtins
 					diagnostics.sqlfluff.with({
 						command = vim.fn.expand("~/miniconda3/envs/dbt/bin/sqlfluff"),
 					}),
@@ -18,7 +20,17 @@ return {
 						command = vim.fn.expand("~/miniconda3/envs/dbt/bin/sqlfluff"),
 					}),
 					formatting.stylua,
-					formatting.xmlformatter,
+					-- extras
+					{
+						name = "xmlformatter",
+						method = null_ls.methods.FORMATTING,
+						filetypes = { "xml" },
+						generator = helpers.formatter_factory({
+							command = "xmlformat",
+							args = { "--indent", "2", "-" },
+							to_stdin = true,
+						}),
+					},
 				},
 			})
 		end,
